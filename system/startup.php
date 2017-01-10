3ad027eaf91676778651a10760129002156e09f6
Azeem Hassni <azeemhassni@users.noreply.github.com>
2017-01-10 23:01:51 +0500

    Removed duplicate ini_set calls
    removed `ini_set('display_errors', 1);`  duplicate line.

--- system/startup.php
+++ system/startup.php
@@ -2,7 +2,6 @@
 define('DS', DIRECTORY_SEPARATOR, true);
 define('BASE_PATH', dirname(__DIR__).DS, TRUE);
 ini_set('display_errors', 1);
-ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
