9c74bd793de6c274af80dba92be525f75539d82e
Mahmoud Elnezamy <m_nezamy@hotmail.com>
2017-01-15 17:58:43 +0200

    Update startup.php

--- system/startup.php
+++ system/startup.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require "app.php";
+require "App.php";
 require "function.php";
 
 $app            = System\App::instance();
